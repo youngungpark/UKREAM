a951c1b021b3fcd41bd594947cc548a2dbdc2d64
park-yeong-ung <youngung2213@gmail.com>
2023-11-02 19:59:23 +0900

    프로젝트 설정

---
+++ src/test/java/com/ukream/UkreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
