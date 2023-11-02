c1af51ea3f1e6d7536f2f09cb34d49c66c0489c7
park-yeong-ung <youngung2213@gmail.com>
2023-11-02 19:59:23 +0900

    프로젝트 설정

---
+++ src/main/java/com/ukream/UkreamApplication.java
@@ -0,0 +1,14 @@
+package com.ukream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkreamApplication.class, args);
+	}
+
+
+}
