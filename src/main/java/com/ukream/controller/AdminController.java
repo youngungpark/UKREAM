912cb45056c6772e8008a82d86d4124b0523a9a1
park-yeong-ung <youngung2213@gmail.com>
2023-11-11 14:54:05 +0900

    테스트

---
+++ src/main/java/com/ukream/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.ukream.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ukream.annotation.LoginCheck;
+import com.ukream.dto.UserDTO;
+import com.ukream.service.AdminService;
+
+@RestController
+@RequestMapping("/admins")
+public class AdminController {
+    @Autowired
+    private AdminService adminService;
+
+    @LoginCheck(type = LoginCheck.UserType.ADMIN)
+    @GetMapping("/usersSddㄴ")
+    public List<UserDTO> findByUsers() {
+        return adminService.findByUsers();
+    }
+}
