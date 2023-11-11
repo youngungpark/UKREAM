219c35cfc67c8e3510171986175d7fcbf2aa8d5f
park-yeong-ung <youngung2213@gmail.com>
2023-11-11 14:12:11 +0900

    테스트

--- src/main/java/com/ukream/controller/AdminController.java
+++ src/main/java/com/ukream/controller/AdminController.java
@@ -18,7 +18,7 @@ public class AdminController {
     private AdminService adminService;
 
     @LoginCheck(type = LoginCheck.UserType.ADMIN)
-    @GetMapping("/users")
+    @GetMapping("/usersS")
     public List<UserDTO> findByUsers() {
         return adminService.findByUsers();
     }
